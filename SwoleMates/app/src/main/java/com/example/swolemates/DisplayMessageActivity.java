3be5ce955c3b443e1f7d13a5b67ce67a1053bbed
einwoko96 <einwoko96@yahoo.com>
2017-03-23 11:40:52 -0500

    DisplayMessageActivity update

--- SwoleMates/app/src/main/java/com/example/swolemates/DisplayMessageActivity.java
+++ SwoleMates/app/src/main/java/com/example/swolemates/DisplayMessageActivity.java
@@ -1,8 +1,8 @@
 package com.example.swolemates;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
