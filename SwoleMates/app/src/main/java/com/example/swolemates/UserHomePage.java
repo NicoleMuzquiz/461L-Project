f09e5450716f016d92a16db5860fdf1dca42c2c2
Samuel Patterson <samuelvpatterson@gmail.com>
2017-04-29 10:59:08 -0500

    almost finished with homepage

--- SwoleMates/app/src/main/java/com/example/swolemates/UserHomePage.java
+++ SwoleMates/app/src/main/java/com/example/swolemates/UserHomePage.java
@@ -2,6 +2,11 @@ package com.example.swolemates;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+import com.example.settings.DefaultSettingsActivity;
 
 public class UserHomePage extends AppCompatActivity {
 
@@ -9,5 +14,49 @@ public class UserHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home_page);
+
+        /* Button for moving to select sport page */
+        Button selectSportButton = (Button) findViewById(R.id.sportbutton);
+        selectSportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SelectSport.class);
+                startActivity(intent);
+            }
+        });
+
+        /* Button for moving to view matches page */
+        Button matchButton = (Button) findViewById(R.id.matchbutton);
+        selectSportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SelectSport.class);
+                startActivity(intent);
+            }
+        });
+
+        /* button for moving to settings page */
+        Button settingsButton = (Button) findViewById(R.id.settingsbutton);
+        selectSportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), DefaultSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /* button for moving to logout page */
+        Button logoutButton = (Button) findViewById(R.id.logoutbutton);
+        selectSportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(getApplicationContext(), SelectSport.class);
+                startActivity(intent);
+            }
+        });
     }
 }
